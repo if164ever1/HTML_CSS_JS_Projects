a21027bed45623f3d1f90263b755467590d97009
if164ever1 <acere5ihor@gmail.com>
2022-08-02 19:42:33 +0200

    refactored validation

--- formValidator/script.js
+++ formValidator/script.js
@@ -1,22 +1,41 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const form      =   document.getElementById('form');
+const username  =   document.getElementById('username');
+const password  =   document.getElementById('password');
+const password2 =   document.getElementById('password2');
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+};
 
 function showError(input, message){
     const formControl = input.parentElement;
-
     formControl.className = 'form-control error';
-
     const small = formControl.querySelector('small');
-
     small.innerText = message;
 }
 
 function showSuccess(input){
     const formControl = input.parentElement;
-
     formControl.className = 'form-control success';
+}
+
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+
+function checkRequiredField(inputTextArray){
+    inputTextArray.forEach(element => {
+        if(element.value.trim() === ''){
+            showError(element, `${getFieldName(element)} is required`);
+        } else {
+            showSuccess(element);
+        }
+    });
 }
 
 
@@ -24,40 +43,35 @@ function showSuccess(input){
 form.addEventListener('submit', function(e){
     e.preventDefault();
     
-    if(username.value === ''){
-        showError(username, 'Username is requred');
-    }else {
-        showSuccess(username);
-    }
+    // if(username.value === ''){
+    //     showError(username, 'Username is requred');
+    // }else {
+    //     showSuccess(username);
+    // }
 
-    const validateEmail = (email) => {
-        return String(email)
-          .toLowerCase()
-          .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          );
-      };
+    
+    // if(email.value === ''){
+    //     showError(email, 'Email is requred');
+    // }else if(!validateEmail(email.value)){
+    //     showError(email, 'Email is not valid');
+    // }
+    // else {
+    //     showSuccess(email);
+    // }
 
-    if(email.value === ''){
-        showError(email, 'Email is requred');
-    }else if(!validateEmail(email.value)){
-        showError(email, 'Email is not valid');
-    }
-    else {
-        showSuccess(email);
-    }
+    // if(password.value === ''){
+    //     showError(password, 'Password is requred');
+    // }else {
+    //     showSuccess(password);
+    // }
 
-    if(password.value === ''){
-        showError(password, 'Password is requred');
-    }else {
-        showSuccess(password);
-    }
+    // if(password2.value === ''){
+    //     showError(password2, 'Password is requred');
+    // }else {
+    //     showSuccess(password2);
+    // }
 
-    if(password2.value === ''){
-        showError(password2, 'Password is requred');
-    }else {
-        showSuccess(password2);
-    }
+    checkRequiredField([username, email, password, password2]);
 })
 
 
