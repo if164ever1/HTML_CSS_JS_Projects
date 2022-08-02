bcc4e3f2e794e45228330062d19c3a5f5c488f21
if164ever1 <acere5ihor@gmail.com>
2022-08-02 20:09:20 +0200

    make refactoring

--- formValidator/script.js
+++ formValidator/script.js
@@ -3,13 +3,6 @@ const username  =   document.getElementById('username');
 const password  =   document.getElementById('password');
 const password2 =   document.getElementById('password2');
 
-const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-};
 
 function showError(input, message){
     const formControl = input.parentElement;
@@ -38,40 +31,41 @@ function checkRequiredField(inputTextArray){
     });
 }
 
+function checkLengthField(input, min, max){
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} is must be at least ${min} characters`);
+    } else if(input.value.length > max){
+        showError(input, `${getFieldName(input)} is must be less than ${max} characters`);
+    }else {
+        showSuccess(input);
+    }
+}
 
+function checkValidEmail(input){
+    const regEx = 
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      
+      if(regEx.test(input.value)){
+        showSuccess(input);
+      }else{
+        showError(input, 'Email is not valid');
+      }
+}
+
+function checkPasswordMatch(password1, pasword2){
+    if(password1.value !== pasword2.value){
+        showError(pasword2, 'Password is not to mutch')
+    }
+}
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
     
-    // if(username.value === ''){
-    //     showError(username, 'Username is requred');
-    // }else {
-    //     showSuccess(username);
-    // }
-
-    
-    // if(email.value === ''){
-    //     showError(email, 'Email is requred');
-    // }else if(!validateEmail(email.value)){
-    //     showError(email, 'Email is not valid');
-    // }
-    // else {
-    //     showSuccess(email);
-    // }
-
-    // if(password.value === ''){
-    //     showError(password, 'Password is requred');
-    // }else {
-    //     showSuccess(password);
-    // }
-
-    // if(password2.value === ''){
-    //     showError(password2, 'Password is requred');
-    // }else {
-    //     showSuccess(password2);
-    // }
-
     checkRequiredField([username, email, password, password2]);
+    checkLengthField(username, 3, 24);
+    checkLengthField(password, 3, 24);
+    checkValidEmail(email);
+    checkPasswordMatch(password, password2);
 })
 
 
