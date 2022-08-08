efe19a1d4f0a6b294ffc2187928a7b219ce31434
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2022-08-08 13:33:09 +0200

    implement a functions for localstorage

--- cinemaHallSeetingBook/script.js
+++ cinemaHallSeetingBook/script.js
@@ -11,13 +11,25 @@ function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     const selectedSeatsCount = selectedSeats.length;
 
+    const seatsIndex = [...selectedSeats].map(function(seat){
+        return [...seats].indexOf(seat);
+    })
+
+    localStorage.setItem(selectedSeats, JSON.stringify(seatsIndex));
+
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
-    //console.log(selectedSeatsCount);    
+     
+}
+
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex)
+    localStorage.setItem('selectedMoviePrice', moviePrice);
 }
 
 movieSelect.addEventListener('change', function(e){
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 
